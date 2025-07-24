13d0eac7dba1be608d55b0a2c1ad2b666ed004d5
théodore clément <213218350+thclemnt@users.noreply.github.com>
2025-07-24 09:53:56 +0200

    fix(proxy): recursive html rewriting

--- front/proxy.php
+++ front/proxy.php
@@ -120,7 +120,7 @@ $rewriteCallback = function ($matches) use ($proxySelf, $path) {
     
     $resolvedPath = resolve_relative_url($originalUrl, $path);
     
-    $newUrl = $proxySelf . '?path=' . urlencode($resolvedPath);
+    $newUrl = $proxySelf . '?path=' . urlencode($resolvedPath) . (isSecure() ? '&https' : '');
     
     // For HTML: $matches[1] = '<a href=', $matches[2] = '"', $matches[4] = '"'
     // For CSS:  $matches[1] = 'url(', $matches[2] = '"', $matches[4] = '")'
